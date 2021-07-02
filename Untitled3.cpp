7a62efb59697a378bb82711d276793839ba5822a
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-07-02 21:57:40 +0500

    Add files via upload

---
+++ Untitled3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,c,sum,avg;
+	cout<<"enter the value of a:"<<a<<endl;
+	cout<<"enter the value of b:"<<b<<endl;
+	cout<<"enter the value of c:"<<c<<endl;
+	sum=a+b+c;
+	avg=(sum)/3;
+	cout<<"the sum of three value is"<<sum<<endl;
+	cout<<"the avg of three value is"<<avg<<endl;
+	return(0);
+}
